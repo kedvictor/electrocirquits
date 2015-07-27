9cf2f6ec5a05cf16e6a190ba84de5f9b6bc730b2
kedvictor <kedvictor@gmail.com>
2015-07-27 21:55:48 +0300

    students #1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,51 @@ class StudentsController < ApplicationController
   
   layout 'backend'
   
-  def index    
+  before_action :set_group
+  
+  def index
+    get_students_groups
+  end
+  
+  def new
+    @student = Student.new group_id: @group_id
+    render action: :form
+  end
+  
+  def create
+    
+    redirect_to action: :index
+  end
+  
+    
+  def edit
+    @student = Student.find params[:id]
+    render action: :form
+  end
+  
+  def update
+    
+    redirect_to action: :index
+  end
+  
+  def destroy
+    
+    redirect_to action: :index
+  end
+  
+  private
+  
+  def set_group
+    @group_id = params[:group_id]
+  end
+  
+  def get_students_groups
+    if @group_id
+      group = Group.find_by_id @group_id
+      @groups = [group]
+    else
+      @groups = Group.all
+    end
   end
   
 end
