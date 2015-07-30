879b9041cbd9dffacec8172d83d1acb05abf3019
kedvictor <kedvictor@gmail.com>
2015-07-30 15:20:30 +0300

    preview

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,18 +13,17 @@ class StaticPagesController < ApplicationController
     render layout: 'frontend'
   end
   
-  def generate_page
-    set_variables
-    
-    uri = URI.parse("http://electrocirquits.herokuapp.com/backend/preview")
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    puts '-------start request-------------'
-    response = http.request(request)
-    puts '-----------end request-------------'
-    str = response.body.force_encoding('UTF-8')
+  def generate_page    
+#    uri = URI.parse("http://electrocirquits.herokuapp.com/backend/preview")
+#    http = Net::HTTP.new(uri.host, uri.port)
+#    request = Net::HTTP::Get.new(uri.request_uri)
+#    puts '-------start request-------------'
+#    response = http.request(request)
+#    puts '-----------end request-------------'
+#    str = response.body.force_encoding('UTF-8')
     #str = Net::HTTP.get('http://localhost:3000', '/backend/preview')
-    #str = render_to_string 'preview', layout: 'frontend'
+    #str = render_to_string 'preview', layout: 'frontend
+    str = open("http://electrocirquits.herokuapp.com/backend/preview") {|f| f.read }
     File.open("app/views/static_pages/index.html", "w") do |file|
       file.write str
     end
