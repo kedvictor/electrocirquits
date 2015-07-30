b9e9b0190606e628cd9e266c4041336babff16ff
kedvictor <kedvictor@gmail.com>
2015-07-30 15:53:43 +0300

    preview

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   
-  require "open-uri"
-  
   skip_before_action :authenticate, only: [:index, :preview]
   
   def index
@@ -13,23 +11,11 @@ class StaticPagesController < ApplicationController
   end
   
   def generate_page    
-#    uri = URI.parse("http://electrocirquits.herokuapp.com/backend/preview")
-#    http = Net::HTTP.new(uri.host, uri.port)
-#    request = Net::HTTP::Get.new(uri.request_uri)
-#    puts '-------start request-------------'
-#    response = http.request(request)
-#    puts '-----------end request-------------'
-#    str = response.body.force_encoding('UTF-8')
-    #str = Net::HTTP.get('http://localhost:3000', '/backend/preview')
+    set_variables
     str = render_to_string 'preview', layout: 'frontend'
-    #str = open("http://electrocirquits.herokuapp.com/backend/preview") {|f| f.read }
-    puts '-------------------------------------------------------------'
-    puts ActionController::Base.asset_host
     host = ActionController::Base.asset_host
-ActionController::Base.asset_host = request.protocol + request.host_with_port if host.blank?
-    File.open("app/views/static_pages/index.html", "w") do |file|
-      file.write str
-    end
+    ActionController::Base.asset_host = request.protocol + request.host_with_port if host.blank?
+    File.open('app/views/static_pages/index.html', 'w') { |file| file.write str }
     redirect_to root_path
   end
   
